60e09498ca5bea23ce568f8c858419d1da179c74
misha133kv <mihail_fomin_95@mail.ru>
2023-05-31 23:20:53 +0500

    added hover effects

---
+++ src/components/map/Icon.jsx
@@ -0,0 +1,23 @@
+import { MapPinIcon } from '@heroicons/react/24/solid'
+
+
+export default function Icon({ point, desc, index, isActive, onMouseOver, onMouseOut }) {
+
+
+	return (
+		<div
+			key={point.name}
+			style={{ left: `${point.x}%`, top: `${point.y}%` }}
+			className='absolute w-6 h-6 text-red-700 transition-all duration-100 -translate-y-full cursor-pointer hover:text-red-600 hover:scale-125'
+			// onClick={handlePointClick}
+			index={index}
+			onMouseOver={() => onMouseOver(index)}
+			onMouseOut={() => onMouseOut()}
+		>
+			<MapPinIcon />
+			{isActive &&
+				<div className='w-24 text-sm text-center text-gray-900 bg-white rounded'>{desc}</div>
+			}
+		</div>
+	)
+}
--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react'
 import initialPoints from '../../../public/model'
 import { MapPinIcon } from '@heroicons/react/24/solid'
-
-
+import Icon from './Icon';
 
 export default function Map({ logout }) {
+	const [activeIndex, setActiveIndex] = React.useState(null);
 	const [points, setPoints] = React.useState(() => {
 		const str = localStorage.getItem('points')
 		if (!!str) {
@@ -15,9 +15,17 @@ export default function Map({ logout }) {
 		}
 	})
 
+	function handleIconMouseOver(index) {
+		setActiveIndex(index);
+	}
+	function handleIconMouseOut() {
+		setActiveIndex(null);
+	}
+
 	const handlePointClick = (e) => {
 		e.preventDefault()
 		// TODO open modal with points data
+
 	}
 
 	const handleLogOut = (e) => {
@@ -29,32 +37,25 @@ export default function Map({ logout }) {
 
 		<div className="container relative h-screen mx-auto ">
 			<menu className="flex justify-between w-full mt-3 border-b-2 border-gray-400 ">
-				<div>
-					<button>Add point</button>
-				</div>
-				<button
-					onClick={handleLogOut}
-				>
+				<button>Add point</button>
+				<button onClick={handleLogOut}>
 					Log out
 				</button>
 			</menu>
-			<div className='relative w-[1000px] h-[1000px] flex justify-center'>
-				<img
-					style={{}}
-					className='w-full h-full' src='../../../public/tutzing.svg' />
-
-				{points.map(point => {
-					return <div
-						style={{ left: `${point.x}%`, top: `${point.y}%` }}
-						className='absolute w-4 h-4'
-						onClick={handlePointClick}
-					>
-						<MapPinIcon />
-					</div>
-				})}
-
+			<div className='relative w-[1000px] h-[1000px]'>
+				<img className='w-full h-full' src='../../../public/tutzing.svg' />
+				{points.map((point, index) => (
+					<Icon
+						key={index}
+						point={point}
+						desc={`${point.name}`}
+						index={index}
+						isActive={activeIndex === index}
+						onMouseOver={handleIconMouseOver}
+						onMouseOut={handleIconMouseOut}
+					/>
+				))}
 			</div>
 		</div>
-
 	)
 }
